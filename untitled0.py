44d80f3ef09c75f9cccffb83b50f3f9a139afcd1
player6 <mathummaldhruv@gmail.com>
2026-05-06 19:12:01 +0300

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 18:39:53 2026
+
+@author: dhruv
+"""
+
+import cv2
+
+image = cv2.imread('/home/dhruv/Pictures/painting.png')
+
+cv2.namedWindow('Loaded Image',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Loaded Image',800,500)
+
+
+cv2.imshow('Loaded Image',image)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+print(f'Image Dimentions: {image.shape}')
